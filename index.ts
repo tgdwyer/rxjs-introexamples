09d21ed50559b0a9f78dd199626e2585eed5c64f
tgdwyer <tgdwyer@gmail.com>
2023-06-30 17:16:32 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { of, range, fromEvent, merge, zip, startWith } from 'rxjs'; 
-import { last,filter,scan,map,mergeMap,take, takeUntil } from 'rxjs/operators';
+import { of, range, fromEvent, merge, zip } from 'rxjs'; 
+import { last,filter,scan,map,mergeMap } from 'rxjs/operators';
 
 console.log("Observable of parameters:")
 
@@ -60,91 +60,3 @@ mouse$.pipe(
 merge(key$.pipe(map(e=>e.key)),
       mouse$.pipe(map(_=>"Mouse Click!"))
 ).subscribe(console.log)
-
-/**
- * Add a drag and drop behaviour to an SVG rectangle.
- * Warning, reading the DOM in the function passed to the first
- * map makes it impure!  See dragrect2 below for the solution.
- */
-function dragrect1() {
-  const svg = document.getElementById("svgCanvas")!;
-  const rect = document.getElementById("draggableRect")!;
-
-  const mousedown = fromEvent<MouseEvent>(rect,'mousedown'),
-        mousemove = fromEvent<MouseEvent>(svg,'mousemove'),
-        mouseup = fromEvent<MouseEvent>(svg,'mouseup');
-
-  mousedown
-    .pipe(
-      map(({clientX, clientY}) => ({
-        mouseDownXOffset: Number(rect.getAttribute('x')) - clientX,
-        mouseDownYOffset: Number(rect.getAttribute('y')) - clientY
-      })),
-      mergeMap(({mouseDownXOffset, mouseDownYOffset}) =>
-        mousemove
-          .pipe(
-            takeUntil(mouseup),
-            map(({clientX, clientY}) => ({
-                x: clientX + mouseDownXOffset,
-                y: clientY + mouseDownYOffset
-              })))))
-   .subscribe(({x, y}) => {
-     rect.setAttribute('x', String(x))
-     rect.setAttribute('y', String(y))
-   });
-}
-
-/**
- * Tidy up the stream logic such that all state is managed
- * by a pure function passed to scan.
- */
-function dragrect2() {
-  interface Point { readonly x:number, readonly y:number }
-  abstract class MousePosEvent implements Point { 
-    readonly x:number; readonly y:number;
-    constructor(e:MouseEvent) {
-      [this.x,this.y] = [e.clientX, e.clientY]
-    } 
-  }
-  class DownEvent extends MousePosEvent {}
-  class DragEvent extends MousePosEvent {}
-  type State = Readonly<{
-    rect:Point,
-    downrect:Point,
-    downpos:Point
-  }>
-  const svg = document.getElementById("svgCanvas")!;
-  const rect = document.getElementById("draggableRect")!;
-
-  const mousedown = fromEvent<MouseEvent>(rect,'mousedown'),
-        mousemove = fromEvent<MouseEvent>(svg,'mousemove'),
-        mouseup = fromEvent<MouseEvent>(svg,'mouseup');
-  const initRectPos:Point = {
-    x:Number(rect.getAttribute('x')),
-    y:Number(rect.getAttribute('y'))
-  }
-
-  mousedown
-    .pipe(
-      mergeMap(mouseDownEvent =>
-        mousemove.pipe(
-          takeUntil(mouseup),
-          map(mouseDragEvent=>new DragEvent(mouseDragEvent)),
-          startWith(new DownEvent(mouseDownEvent)))),
-      scan((a:State,e:MousePosEvent)=> 
-        e instanceof DownEvent
-        ? {...a,
-            downrect:a.rect,
-            downpos:{x:e.x,y:e.y} }
-        : {...a,
-            rect:{
-              x:e.x + a.downrect.x - a.downpos.x,
-              y:e.y + a.downrect.y - a.downpos.y} }
-      ,<State>{ rect:initRectPos })
-    )
-   .subscribe(e => {
-     rect.setAttribute('x', String(e.rect.x))
-     rect.setAttribute('y', String(e.rect.y))
-   });
-}
-dragrect2()
